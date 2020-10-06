3884e3e28509c21af9f99dc9374ee20bf9472794
rafal.miecznik <rafal.miecznik@consult.red>
2020-10-06 17:33:04 +0200

    screen width adapatation added

--- main.js
+++ main.js
@@ -1,27 +1,67 @@
-//var slides = [
-//  "afr_history.jpg",
-//  "data_tracing.jpg",
-//  "emubt_app.jpg",
-//  "fuelve.jpg",
-//  "knock_history.jpg",
-//  "s1.jpg",
-//  "s2.jpg",
-//  "tunerpro1.jpg",
-//];
-//
-//var slides_descriptions = [
-//  "TunerPro support, AFR reading",
-//  "TunerPro hit data tracing",
-//  "Storage of up to 3 different binaries",
-//  "Fuel, Ignition and other maps are editable",
-//  "Knock tracing and monitoring",
-//  "Price: 100EUR + 10EUR shipping",
-//  "Board pemamently replaces old EEPROM chip",
-//  "Support for TunerPro"
-//];
 
+window.onscroll = function() {checkScroll();};
+window.onresize = function() {checkResize();};
 
 var top_banner = document.getElementsByClassName("top_bar_container")[0];
+var navbar = document.getElementsByClassName("topnav")[0];
+var top_bar = document.getElementsByClassName("top_bar_container")[0];
+var floating_links = document.getElementById("links");
+floating_links.style.left = "-200px";
+
+var main_grid = document.getElementById("main_grid");
+var main_grid_default_style = main_grid.style;
+var alignement_switch_width = 1500;
+
+checkScroll();
+adapt_to_screen(alignement_switch_width);
+
+
+
+function checkResize() {
+    adapt_to_screen(alignement_switch_width);
+    checkScroll();
+    //p = document.getElementsByTagName('p');
+    //for (var i=0; i<p.length; i++){
+    //  p[i].style.fontSize = "20px";
+    //}
+    //main.classList.remove("main_grid");
+    //main.classList.add("simple_grid");
+}
+
+function checkScroll() {
+
+  top_banner_rect = top_banner.getBoundingClientRect();
+  floating_links_rect = floating_links.getBoundingClientRect();
+  var floating_links_overlap = top_banner_rect.top + top_banner_rect.height - floating_links_rect.top;
+
+  // floating links hiding
+  if(floating_links_overlap > 0){
+    floating_links.style.left = "-200px";
+  }
+  else {
+    floating_links.style.left = "0px";
+  } 
+  //sticky navbar
+  if (window.pageYOffset > 0) {
+    navbar.classList.add("sticky");
+    top_bar.style.top = "-200px";
+  } else {
+    navbar.classList.remove("sticky");
+    top_bar.style.top = "0px";
+  }
+}
+
+function adapt_to_screen (switch_width) {
+    console.log("adapt" + switch_width + ' ' + window.window.innerWidth);
+    var width = window.window.innerWidth;
+    if(width <= switch_width){
+      main_grid.style.gridTemplateColumns = "1fr";  
+    }
+    else{
+      main_grid.style = main_grid_default_style;  
+    }
+}
+
 
 //var max_slides = slides.length;
 //var slide_num = 0;
@@ -49,60 +89,3 @@ var top_banner = document.getElementsByClassName("top_bar_container")[0];
 //    slide_text.text = slides_descriptions[slide_num];
 //    }
 
-
-window.onscroll = function() {checkScroll();};
-window.onresize = function() {checkResize();};
-
-var main = document.getElementById("main");
-
-var navbar = document.getElementsByClassName("topnav")[0];
-var sticky = navbar.offsetTop;
-
-var top_bar = document.getElementsByClassName("top_bar_container")[0];
-
-var floating_links = document.getElementById("links");
-floating_links.style.left = "-200px";
-checkScroll();
-
-function checkResize() {
-    checkScroll();
-    //p = document.getElementsByTagName('p');
-    //for (var i=0; i<p.length; i++){
-    //  p[i].style.fontSize = "20px";
-    //}
-    //main.classList.remove("main_grid");
-    //main.classList.add("simple_grid");
-}
-
-function checkScroll() {
-
-  top_banner_rect = top_banner.getBoundingClientRect();
-  floating_links_rect = floating_links.getBoundingClientRect();
-  var floating_links_overlap = top_banner_rect.top + top_banner_rect.height - floating_links_rect.top;
-
-  // floating links hiding
-  if(floating_links_overlap > 0){
-    floating_links.style.left = "-200px";
-  }
-  else {
-    floating_links.style.left = "0px";
-  }
-  
-  
-  //sticky navbar
-  if (window.pageYOffset > 0) {
-    navbar.classList.add("sticky");
-    top_bar.style.top = "-200px";
-  } else {
-    navbar.classList.remove("sticky");
-    top_bar.style.top = "0px";
-  }
-  
-  
-  //  if (window.scrollY > 300) {
-      //setTimeout(function() {document.getElementById("hidden_bar").style.left = "-200px";}, 5000);
-  //  }
-  //  else {
-      //document.getElementById("hidden_bar").style.left = "-200px";
-  //  }
-}
