52d04b580caafb2327ac07526b93c8bd6d4f8242
rafal <ravmiecznk@gmail.com>
2022-09-25 13:37:29 +0200

    update

--- main.js
+++ main.js
@@ -1,44 +1,39 @@
 
 
+function includeHTML() {
+  var z, i, elmnt, file, xhttp;
+  /* Loop through a collection of all HTML elements: */
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    /*search for elements with a certain atrribute:*/
+    file = elmnt.getAttribute("w3-include-html");
+    if (file) {
+      /* Make an HTTP request using the attribute value as the file name: */
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+          /* Remove the attribute, and call this function once more: */
+          elmnt.removeAttribute("w3-include-html");
+          includeHTML();
+        }
+      }
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      /* Exit the function: */
+      return;
+    }
+  }
+}
+
+includeHTML();
+
 window.onload = function() {checkResize();};
 window.onresize = function() {checkResize();};
 window.onscroll = function() {checkScroll();};
 
-window.onload = function() {
-	var elements = document.getElementsByTagName('*'),
-		i;
-	for (i in elements) {
-		if (elements[i].hasAttribute && elements[i].hasAttribute('data-include')) {
-			fragment(elements[i], elements[i].getAttribute('data-include'));
-		}
-	}
-	function fragment(el, url) {
-		var localTest = /^(?:file):/,
-			xmlhttp = new XMLHttpRequest(),
-			status = 0;
-
-		xmlhttp.onreadystatechange = function() {
-			/* if we are on a local protocol, and we have response text, we'll assume
- *  				things were sucessful */
-			if (xmlhttp.readyState == 4) {
-				status = xmlhttp.status;
-			}
-			if (localTest.test(location.href) && xmlhttp.responseText) {
-				status = 200;
-			}
-			if (xmlhttp.readyState == 4 && status == 200) {
-				el.outerHTML = xmlhttp.responseText;
-			}
-		};
-    
-		try { 
-			xmlhttp.open("GET", url, true);
-			xmlhttp.send();
-		} catch(err) {
-			/* todo catch error */
-		}
-	}
-};
 
 var top_banner = document.getElementsByClassName("top_bar_container")[0];
 var navbar = document.getElementsByClassName("topnav")[0];
@@ -56,7 +51,6 @@ var p_tag_default_fontSize = document.getElementsByTagName('p')[0].style.fontSiz
 checkScroll();
 checkResize();
 GetLatestReleaseInfo();
-
 
 
 function checkResize() {
